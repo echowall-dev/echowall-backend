5dd66dc85e6ef00943841aaa24a6c74112d3a3ef
Kakit Ho <hkk565@gmail.com>
2017-08-14 18:44:14 +0800

    Update models for properties validation

--- common/mixins/media.js
+++ common/mixins/media.js
@@ -2,7 +2,7 @@ module.exports = function(Model, options) {
   // Model is the model class
   // options is an object containing the config properties from model definition
   Model.defineProperty('name', {type: String, required: true});
-  Model.defineProperty('storage', {type: String});
+  Model.defineProperty('storage', {type: String, default: null});
   Model.defineProperty('format', {type: String, required: true});
-  Model.defineProperty('url', {type: String});
+  Model.defineProperty('url', {type: String, default: null});
 };
--- common/models/comment.js
+++ common/models/comment.js
@@ -1,13 +1,24 @@
 'use strict';
 
 module.exports = function(Comment) {
+  // To ensure the comment is not empty when the type is "text"
   Comment.observe('before save', (ctx, next) => {
-    if (ctx.instance.type === 'text' && (ctx.instance.text === null || ctx.instance.text === '')) {
-      let err = new Error('The comment is empty');
-      err.statusCode = 422;
-      next(err);
+    if (ctx.instance) {
+      if (ctx.instance.type === 'text' && (ctx.instance.message === null || ctx.instance.message === '')) {
+        let err = new Error('The comment is empty');
+        err.statusCode = 422;
+        next(err);
+      } else {
+        next();
+      }
     } else {
-      next();
+      if (ctx.data.type === 'text' && (ctx.data.message === null || ctx.data.message === '')) {
+        let err = new Error('The comment is empty');
+        err.statusCode = 422;
+        next(err);
+      } else {
+        next();
+      }
     }
   });
 };

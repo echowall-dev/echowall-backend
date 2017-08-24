cbd137abe22acfb4edc35ddb45eab7f66da9b0e6
Kakit Ho <hkk565@gmail.com>
2017-08-25 02:59:23 +0800

    Update models for URL generation

--- common/models/post.js
+++ common/models/post.js
@@ -1,5 +1,21 @@
 'use strict';
 
+let appPros = require('../app-properties');
+let postDomain = appPros.hostDomain + 'post/';
+
 module.exports = function(Post) {
+  Post.observe('before save', (ctx, next) => {
+    if (ctx.instance) {
+      if (ctx.instance.url === null) {
+        ctx.instance.url = postDomain + ctx.instance.uuid;
+      }
+    } else {
+      if (ctx.data.url === null) {
+        ctx.data.url = postDomain + ctx.data.uuid;
+      }
+    }
+    next();
+  });
+
   Post.validatesUniquenessOf('url');
 };

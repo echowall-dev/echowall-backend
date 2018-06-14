e9b56cead0487720d5840aaaef2ad48098db9ea6
Kakit Ho <hkk565@gmail.com>
2018-06-14 23:08:43 +0800

    Declare remote-methods for user actions in Relationship model

--- common/models/relationship.js
+++ common/models/relationship.js
@@ -1,95 +1,51 @@
 'use strict';
 
 module.exports = function(Relationship) {
-  // Whenever a user followed another user,
-  // if the target user is also following this user,
-  // they become friends automatically
-  Relationship.observe('after save', (ctx, next) => {
-    const { Account } = app.models;
+  /**
+   * To let a user follow another user
+   * @param {string} subjectUserId The ID of subject user
+   * @param {string} objectUserId The ID of object user
+   * @param {Function(Error, boolean)} callback
+   */
+  Relationship.follow = function(subjectUserId, objectUserId, callback) {
+    let success;
+    // TODO
+    callback(null, success);
+  };
 
-    if (ctx.isNewInstance && ctx.instance.status === 'follow') {
-      // Find if the target user is also following this user
-      Relationship.findOne({
-        where: {
-          subjectUserId: ctx.instance.objectUserId,
-          objectUserId: ctx.instance.subjectUserId,
-          status: 'follow'
-        }
-      }, (err, foundRelationship) => {
-        if (err) {
-          console.error(err);
-          return next(err);
-        }
+  /**
+   * To let a user unfollow another user
+   * @param {string} subjectUserId The ID of subject user
+   * @param {string} objectUserId The ID of object user
+   * @param {Function(Error, boolean)} callback
+   */
+  Relationship.unfollow = function(subjectUserId, objectUserId, callback) {
+    let success;
+    // TODO
+    callback(null, success);
+  };
 
-        // Assume they are friends first
-        let mutualStatus = 'friend';
+  /**
+   * To let a user block another user
+   * @param {string} subjectUserId The ID of subject user
+   * @param {string} objectUserId The ID of object user
+   * @param {Function(Error, boolean)} callback
+   */
+  Relationship.block = function(subjectUserId, objectUserId, callback) {
+    let success;
+    // TODO
+    callback(null, success);
+  };
 
-        // The target user is indeed also following this user
-        if (foundRelationship) {
-          // Change the status of these two users to 'friend'
-          Relationship.updateAll({
-            // This argument itself is a where filter so no 'where:' is needed
-            // 'inq:' is faster than 'or:' on fields which are indexes
-            id: {inq: [foundRelationship.id, ctx.instance.id]}
-          }, {
-            status: 'friend'
-          }, (err, info) => {
-            if (err) {
-              console.error(err);
-              return next(err);
-            }
-            // console.log(info);
-          });
-        } else {
-          // The target user is not following this user
-          mutualStatus = 'follow';
-        }
-
-        if (mutualStatus === 'friend') {
-          Account.findById(ctx.instance.subjectUserId, (err, oldAccount) => {
-            if (err) return next(err);
-            oldAccount.updateAttributes({
-              friendCount: oldAccount.friendCount + 1
-            }, (err, newAccount) => {
-              if (err) return next(err);
-            });
-          });
-
-          Account.findById(ctx.instance.objectUserId, (err, oldAccount) => {
-            if (err) return next(err);
-            oldAccount.updateAttributes({
-              friendCount: oldAccount.friendCount + 1
-            }, (err, newAccount) => {
-              if (err) return next(err);
-            });
-          });
-        } else {
-          Account.findById(ctx.instance.subjectUserId, (err, oldAccount) => {
-            if (err) return next(err);
-            oldAccount.updateAttributes({
-              followeeCount: oldAccount.followeeCount + 1
-            }, (err, newAccount) => {
-              if (err) return next(err);
-            });
-          });
-
-          Account.findById(ctx.instance.objectUserId, (err, oldAccount) => {
-            if (err) return next(err);
-            oldAccount.updateAttributes({
-              followerCount: oldAccount.followerCount + 1
-            }, (err, newAccount) => {
-              if (err) return next(err);
-            });
-          });
-        }
-
-        next();
-      });
-    }
-  });
-
-  // TODO: update followeeCount, followerCount and friendCount for block status
-
-  // TODO: handle delete
-  // Relationship.observe('after delete', (ctx, next) => {});
+  /**
+   * To let a user unblock another user
+   * @param {string} subjectUserId The ID of subject user
+   * @param {string} objectUserId The ID of object user
+   * @param {Function(Error, boolean)} callback
+   */
+  Relationship.unblock = function(subjectUserId, objectUserId, callback) {
+    let success;
+    // TODO
+    callback(null, success);
+  };
 };
